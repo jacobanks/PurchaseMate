3f746b5590e079b4bd1ab829d53dcc5aef468992
Jacob Banks <jacobanks@gmail.com>
2015-06-28 16:21:26 -0400

    Commented out get request in viewDidLoad

--- PurchaseMate/ViewController.m
+++ PurchaseMate/ViewController.m
@@ -30,7 +30,7 @@
 {
     [super viewDidLoad];
     
-    [self getDataFromOutPan:[NSString stringWithFormat:@"https://www.outpan.com/api/get-product.php?apikey=cbf4f07abd482df99358395a75b6340a&barcode=9781118692288"]];
+//    [self getDataFromOutPan:[NSString stringWithFormat:@"https://www.outpan.com/api/get-product.php?apikey=cbf4f07abd482df99358395a75b6340a&barcode=9781118692288"]];
 
     _highlightView = [[UIView alloc] initWithFrame:CGRectMake(62.5, self.view.center.y - 90, 250, 125)];
     _highlightView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin|UIViewAutoresizingFlexibleBottomMargin;
