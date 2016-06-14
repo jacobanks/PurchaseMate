e636121856a611b67e69089896cde6a7cbe14ee1
Jacob Banks <jacobanks@gmail.com>
2016-06-14 12:09:47 -0700

    Removed awake from nib

--- PurchaseMate/scannedTableViewCell.m
+++ PurchaseMate/scannedTableViewCell.m
@@ -10,10 +10,6 @@
 
 @implementation scannedTableViewCell
 
-- (void)awakeFromNib {
-    // Initialization code
-}
-
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 
